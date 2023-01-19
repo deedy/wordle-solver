c09f4ab3601235e514e8ff116cc4c4c459c94653
Levy Nunes <levy.vix@gmail.com>
2023-01-19 18:45:05 -0300

    f-strings use and code quality fixes

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ SHOW = 'show'
 EVAL = 'eval'
 GEN_TREE = 'gen_tree'
 
+
 def play(game_config: Dict[str, str]):
     hidden_word = random.choice(game_config['candidate_set'])
     w = Wordle(hidden_word, config=game_config)
@@ -28,7 +29,8 @@ def play(game_config: Dict[str, str]):
         except Exception as e:
             print(f'Error: {str(e)}')
 
-def save(game_config: Dict[str, str], solver_settings: Dict[str, str], debug: int=0):
+
+def save(game_config: Dict[str, str], solver_settings: Dict[str, str], debug: int = 0):
     N = get_n_from_word_set(solver_settings['guess_set'])
     clues = []
     guesses = 0
@@ -38,7 +40,7 @@ def save(game_config: Dict[str, str], solver_settings: Dict[str, str], debug: in
             print(f'Solved! = {clues[-1][0]}')
             sys.exit()
         print(f'Solver recommends the word [{chosen.upper()}]. There are {lencands} possible words: {cands[:10]}...')
-        error = True 
+        error = True
         while error:
             guess = input('Guess? ')
             if len(guess) != N:
@@ -55,19 +57,20 @@ def save(game_config: Dict[str, str], solver_settings: Dict[str, str], debug: in
         if len(feedback) != N:
             print(f'Error: Result must be {N} length.')
             continue
-        if len(set(feedback + '012')) <= 3:
+        if len(set(f'{feedback}012')) <= 3:
             feedback_parsed = [ord(f) - ord('0') for f in feedback]
-        elif len(set(feedback + '⬛🟨🟩')) <= 3:
+        elif len(set(f'{feedback}⬛🟨🟩')) <= 3:
             reverse_emoji_map = {v: k for k, v in Wordle.EMOJI_MAP.items()}
             feedback_parsed = [reverse_emoji_map[f] for f in feedback]
         else:
-            print(f'Error: Must only be 0, 1, or 2')
+            print('Error: Must only be 0, 1, or 2')
             continue
         guesses += 1
-        clues.append((guess, feedback_parsed)) 
-    print(f'Unsolved!')
+        clues.append((guess, feedback_parsed))
+    print('Unsolved!')
 
-def show(words: List[str], game_config: Dict[str, str], solver_settings: Dict[str, str], debug: int=0):
+
+def show(words: List[str], game_config: Dict[str, str], solver_settings: Dict[str, str], debug: int = 0):
     for word in words:
         try:
             print(f'Word [{word.upper()}]')
@@ -77,8 +80,9 @@ def show(words: List[str], game_config: Dict[str, str], solver_settings: Dict[st
             print(f'Error: {str(e)}')
         print('\n\n')
 
-def solve(game_config: Dict[str, str], solver_settings: Dict[str, str], debug: int=0):
-    if not 'guess_set' in solver_settings: 
+
+def solve(game_config: Dict[str, str], solver_settings: Dict[str, str], debug: int = 0):
+    if 'guess_set' not in solver_settings:
         raise Exception('guess_set not specified in config')
     N = get_n_from_word_set(solver_settings['guess_set'])
     clues = []
@@ -94,20 +98,21 @@ def solve(game_config: Dict[str, str], solver_settings: Dict[str, str], debug: i
         if len(feedback) != N:
             print(f'Error: Result must be {N} length.')
             continue
-        if len(set(feedback + '012')) <= 3:
+        if len(set(f'{feedback}012')) <= 3:
             feedback_parsed = [ord(f) - ord('0') for f in feedback]
-        elif len(set(feedback + '⬛🟨🟩')) <= 3:
+        elif len(set(f'{feedback}⬛🟨🟩')) <= 3:
             reverse_emoji_map = {v: k for k, v in Wordle.EMOJI_MAP.items()}
             feedback_parsed = [reverse_emoji_map[f] for f in feedback]
         else:
-            print(f'Error: Must only be 0, 1, or 2')
+            print('Error: Must only be 0, 1, or 2')
             continue
         guesses += 1
-        clues.append((chosen, feedback_parsed)) 
-    print(f'Unsolved!')
+        clues.append((chosen, feedback_parsed))
+    print('Unsolved!')
 
-def eval(words: List[str], out_file: str, game_config: Dict[str, str], solver_settings: Dict[str, str], debug: int=0):
-    if not 'candidate_set' in solver_settings: 
+
+def eval(words: List[str], out_file: str, game_config: Dict[str, str], solver_settings: Dict[str, str], debug: int = 0):
+    if 'candidate_set' not in solver_settings:
         raise Exception('candidate_set not specified in config')
     candidates = solver_settings['candidate_set']
     print(f'Evaluating on {len(words)} words. Total available candidate words: {len(candidates)}')
@@ -118,7 +123,9 @@ def eval(words: List[str], out_file: str, game_config: Dict[str, str], solver_se
     for x in range(len(words)):
         count = x+1
         if count and count % 10 == 0:
-            print(f'k={count}:\tFailed: {len(fails)}\tAccuracy:{(1 - len(fails)/count)*100:.02f}%\tAvg Attempts: {sum([len(a[1]) for a in attempts])/count:.02f}\tAvg Time: {(time() - start)/count:.03f}s')
+            print(
+                f'k={count}:\tFailed: {len(fails)}\tAccuracy:{(1 - len(fails) / count) * 100:.02f}%\tAvg Attempts: {sum(len(a[1]) for a in attempts) / count:.02f}\tAvg Time: {(time() - start) / count:.03f}s'
+            )
         word = words[x]
         w = Wordle(word, config=game_config, verbose=debug >= 2)
         got_ans, attempt_count, cands = solve_wordle(w, solver_settings=solver_settings, debug=debug)
@@ -130,7 +137,9 @@ def eval(words: List[str], out_file: str, game_config: Dict[str, str], solver_se
     print(f'Failed on: {failed_words}')
     print(f'Distribution of remaining candidates: {Counter([len(f[1]) for f in fails]).most_common()}')
     print(f'Distribution of attempts needed: {Counter([len(a[1]) for a in attempts]).most_common()}')
-    print(f'K={len(words)}:\tFailed: {len(fails)}\tAccuracy:{(1 - len(fails)/len(words))*100:.02f}%\tAvg Attempts: {sum([len(a[1]) for a in attempts])/count:.02f}\tAvg Time: {(time() - start)/count:.03f}s')
+    print(
+        f'K={len(words)}:\tFailed: {len(fails)}\tAccuracy:{(1 - len(fails) / len(words)) * 100:.02f}%\tAvg Attempts: {sum(len(a[1]) for a in attempts) / count:.02f}\tAvg Time: {(time() - start) / count:.03f}s'
+    )
     if out_file:
         print(f'Writing raw results to file [{out_file}]')
         # Potentially create custom out_file name if not provided
@@ -141,6 +150,7 @@ def eval(words: List[str], out_file: str, game_config: Dict[str, str], solver_se
                 f.write(','.join([fail[0], '0', '-'.join(fail[1]), str(len(fail[1]))]) + '\n')
             for attempt in attempts:
                 f.write(','.join([attempt[0], '1', '-'.join(attempt[1]), str(len(attempt[1]))]) + '\n')
+
 
 def main():
     parser = argparse.ArgumentParser(description='Play Wordle')
@@ -184,32 +194,28 @@ def main():
                         help='Value of MAX_GUESSES',
                         default=DEFAULT_MAX_GUESSES,
                         required=False)
-    parser.add_argument('-hard',
-                        '--hard_mode',
-                        action='store_true',
-                        help='Wordle in "hard mode" or not which requires all guesses to conform to the previous clues.',
-                        default=False,
-                        required=False)
+    parser.add_argument(
+        '-hard', '--hard_mode', action='store_true',
+        help='Wordle in "hard mode" or not which requires all guesses to conform to the previous clues.', default=False,
+        required=False)
     parser.add_argument('--dict_file',
                         type=str,
                         help='Dictionary file to read from',
                         default=DEFAULT_DICT,
                         required=False)
-    parser.add_argument('--cand_file',
-                        type=str,
-                        help='Dictionary file to read potential candidate set from. If not provided, dict_file is used.',
-                        default=DEFAULT_CAND_DICT,
-                        required=False)
+    parser.add_argument(
+        '--cand_file', type=str,
+        help='Dictionary file to read potential candidate set from. If not provided, dict_file is used.',
+        default=DEFAULT_CAND_DICT, required=False)
     parser.add_argument('--eval_out_file',
                         type=str,
                         help='A file to write the detailed outputs for the eval to.',
                         default=None,
                         required=False)
-    parser.add_argument('--tree_file',
-                        type=str,
-                        help='A file that contains the pickled vesion of the solution tree for the official wordle configuration.',
-                        default=None,
-                        required=False)
+    parser.add_argument(
+        '--tree_file', type=str,
+        help='A file that contains the pickled vesion of the solution tree for the official wordle configuration.',
+        default=None, required=False)
     args = parser.parse_args()
     N = args.N
     # if args.dict_file != DEFAULT_DICT:
@@ -232,7 +238,7 @@ def main():
             sys.exit()
     else:
         if args.debug >= 1:
-            print(f'Using the same candidate_set as word_set')
+            print('Using the same candidate_set as word_set')
         candidate_set = word_set
     game_config = DEFAULT_GAME_CONFIG
     game_config['max_guesses'] = str(args.guesses)
@@ -252,15 +258,17 @@ def main():
         save(game_config=game_config, solver_settings=solver_settings, debug=args.debug)
     elif args.mode == SHOW:
         if not args.word and not args.random:
-            print(f'Error: Must provide word to solve with -w/--word or -r/--random.')
-            sys.exit() 
+            print('Error: Must provide word to solve with -w/--word or -r/--random.')
+            sys.exit()
         words = args.word.split(',') if args.word else [random.choice(solver_settings['candidate_set'])]
         show(words, game_config=game_config, solver_settings=solver_settings, debug=args.debug)
     elif args.mode == SOLVE:
         solve(game_config=game_config, solver_settings=solver_settings, debug=args.debug)
     elif args.mode == EVAL:
         if not args.eval_out_file:
-            print(f'Running eval. Specify --eval_out_file to write the details of the eval to a file.')
+            print(
+                'Running eval. Specify --eval_out_file to write the details of the eval to a file.'
+            )
         if args.word:
             words = args.word.split(',')
         else:
@@ -274,15 +282,19 @@ def main():
         import pickle
 
         debug = args.debug
-        choose = ['0' for i in range(N)] + ['1' for i in range(N)] + ['2' for i in range(N)]
-        poss =  sorted(list(set([''.join(x) for x in itertools.permutations(choose, r=N)])))
+        choose = (
+            ['0' for _ in range(N)]
+            + ['1' for _ in range(N)]
+            + ['2' for _ in range(N)]
+        )
+        poss = sorted(list({''.join(x) for x in itertools.permutations(choose, r=N)}))
         initclues = [[]]
         solves = []
         count, solved, unsolved = 0, 0, 0
-        while initclues: 
+        while initclues:
             clues = initclues.pop(0)
             count += 1
-            if count % 10 == 0: 
+            if count % 10 == 0:
                 print(f'Count {count} Solved {solved} Unsolved: {unsolved} Clue Num {len(clues)}')
             if len(clues) > int(game_config['max_guesses']):
                 print(f'Unsolved! {len(clues)}')
@@ -294,7 +306,7 @@ def main():
                 # There are no candidates left to guess from
                 continue
             if lencands == 1 and cands[0] in candidate_set:
-                newclue = [c for c in clues]
+                newclue = list(clues)
                 newclue.append((cands[0], [2] * N))
                 print(f'Solved! = {newclue[-1][0]}')
                 solves.append(newclue)
@@ -306,9 +318,8 @@ def main():
                 print(f'Solved! = {clues[-1][0]}')
                 continue
 
-
             for p in poss:
-                newclue = [c for c in clues]
+                newclue = list(clues)
                 newclue.append((chosen, [ord(f) - ord('0') for f in p]))
                 initclues.append(newclue)
 
@@ -322,19 +333,17 @@ def main():
         for solution in solves:
             currdict = solution_tree
             for word, clue in solution:
-                if not word in currdict:
+                if word not in currdict:
                     currdict[word] = {}
                 strclue = ''.join(map(str, clue))
                 if strclue == solved_clue:
                     continue
-                if not strclue in currdict[word]:
+                if strclue not in currdict[word]:
                     currdict[word][strclue] = {}
                 currdict = currdict[word][strclue]
         pickle.dump(solution_tree, file=open('solution_tree.pickle', 'wb'))
 
+
 if __name__ == '__main__':
     main()
     # run tests: python -m unittest
-
-
-
